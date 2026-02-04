94af1ae71c11740d9249528380bad51f3b2f7346
學寫code的LeoWu <144814501+leowu0511@users.noreply.github.com>
2026-02-04 15:27:14 +0800

    upload main.cpp
    Initialize YoloDetector with engine path and handle success or failure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
+#include "YoloDetector.h"
 
+int main() {
+    std::string enginePath = R"(C:\Users\wu096\OneDrive\Desktop\YOLO_cpp\yolo26n.engine)";
+
+    YoloDetector detector(enginePath);
+
+    if (detector.init()) {
+        std::cout << "AOI start success" << std::endl;
+        std::cout << "階段 5 完成 (GPU 緩衝區已建立)" << std::endl;
+    }
+    else {
+        std::cerr << "fail" << std::endl;
+        return -1;
+    }
+
+    // 這裡暫時暫停，讓你看清楚控制台輸出
+    system("pause");
+    return 0;
+}
